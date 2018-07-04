c0e2c37a28ea3efc12ca5aa0fd89f8b7ad008bf7
SKYcrash5991 <christian.koepfelsberger@hotmail.com>
2018-07-04 17:29:02 +0200

    Asteroid, engine,
    asteroid movement
    engine sound tweaked

---
+++ SpaceGame/Assets/Scripts/AsteroidMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidMovement : MonoBehaviour
+{
+    public float speed = 1f;
+    Rigidbody rigidbodyAsteroid;
+    Vector3 movement;
+
+    void Awake()
+    {
+        rigidbodyAsteroid = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        movement.Set(0f, 0f, -speed);
+
+        movement = movement.normalized * speed * Time.deltaTime;
+        rigidbodyAsteroid.MovePosition(transform.position + movement);
+    }
+}
