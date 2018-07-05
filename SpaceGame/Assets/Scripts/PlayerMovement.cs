f14bec66fc389d4d592bbb9d602675ec0521c0c0
Ragnaslox <e1525623@student.tuwien.ac.at>
2018-07-05 08:24:44 +0200

    Added Boundary
    boundary now Destroys Stuff when it flies out of it,
    Also added lightning

--- SpaceGame/Assets/Scripts/PlayerMovement.cs
+++ SpaceGame/Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
 public class PlayerMovement : MonoBehaviour {
 
     public float speed;
     public float smoothing;
     public float tilt;
+    public float xMin, xMax, zMin, zMax;
     Rigidbody rb;
+   
 
     void Start()
     {
+      
         rb = GetComponent<Rigidbody>();
     }
 
@@ -23,7 +29,14 @@ public class PlayerMovement : MonoBehaviour {
 
         rb.velocity = movement * speed*smoothing;
 
-        
+        rb.position = new Vector3
+       (
+           Mathf.Clamp(rb.position.x, xMin, xMax),
+           0.0f,
+           Mathf.Clamp(rb.position.z, zMin, zMax)
+       );
+
+
         rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
        
     }
