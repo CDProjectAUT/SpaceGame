f56a1f4e40ae59b3012ab3c048ba00eb2a78e4d6
SKYcrash5991 <christian.koepfelsberger@hotmail.com>
2018-07-07 10:51:35 +0200

    asteroidmanager
    spawnmanager for asteroids
    other asteroids scripted

---
+++ SpaceGame/Assets/Scripts/AsteroidManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidManager : MonoBehaviour
+{
+   
+    public GameObject asteroid;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn()
+    {
+       
+
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        Instantiate(asteroid, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
